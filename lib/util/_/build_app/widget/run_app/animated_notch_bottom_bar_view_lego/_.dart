4711a03a97da5fc37d46629799053974e4fa9f3f
june lee <melodysdreamj@gmail.com>
2024-07-18 16:48:43 +0900

    Initial commit

---
+++ lib/util/_/build_app/widget/run_app/animated_notch_bottom_bar_view_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverRunApp()
+// Widget coverAnimatedNotchBottomBarViewLego(Widget widget) {
+//   return widget;
+// }
